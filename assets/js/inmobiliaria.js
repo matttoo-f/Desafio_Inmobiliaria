5d09113587ebfa7e6874f7927f6a6156ff64f979
matttoo-f <mato.arttattoo@gmailcom>
2024-01-14 16:47:03 -0300

    subida final

--- assets/js/inmobiliaria.js
+++ assets/js/inmobiliaria.js
@@ -1,6 +1,8 @@
 // Elementos del DOM
 const ventaMuestra = document.getElementById("ventaCasas")
 const alquilerMuestra = document.getElementById("alquilerCasas")
+const totalVenta = document.getElementById("totalVenta")
+const totalAlquiler = document.getElementById("totalAlquiler")
 // Variables
 const propiedadesVenta = [
     {
@@ -184,6 +186,42 @@ const propiedadesAlquiler = [
         pets: true,
 
     },
+    {
+        titulo: 'Apartamento en el centro de la ciudad',
+        imagen: 'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60',
+        descripcion: 'Este apartamento de 2 habitaciones está ubicado en el corazón de la ciudad, cerca de todo.',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2000,
+        smoke: true,
+        pets: true,
+
+    },
+    {
+        titulo: 'Apartamento luminoso con vista al mar',
+        imagen: 'https://images.unsplash.com/photo-1669071192880-0a94316e6e09?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80',
+        descripcion: 'Este hermoso apartamento ofrece una vista impresionante al mar',
+        ubicacion: '456 Ocean Avenue, Seaside Town, CA 56789',
+        habitaciones: 3,
+        baños:3,
+        costo: 2500,
+        smoke: false,
+        pets: true,
+
+    },
+    {
+        titulo: 'Condominio moderno en zona residencial',
+        imagen: 'https://images.unsplash.com/photo-1567496898669-ee935f5f647a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGNvbmRvfGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=1000&q=60',
+        descripcion: 'Este elegante condominio moderno está ubicado en una tranquila zona residencial',
+        ubicacion: '123 Main Street, Anytown, CA 91234',
+        habitaciones: 2,
+        baños: 2,
+        costo: 2500,
+        smoke: false,
+        pets: true,
+
+    },
 ]
 
 // Funciones
@@ -197,58 +235,134 @@ const generarMensajeYClases = (valor, icono, mensajeVerdadero, mensajeFalso, cla
 
     return `<p class="${clase}">${iconoHTML} ${mensaje}</p>`
 }
+//agregar 3 tarjetas Ventas
+function agregarTarjetasVenContenedor(datos, contenedor) {
+    for (let i = 0; i < 3 && i < datos.length; i++) {
+        const datosApartamento = datos[i];
 
-// iterar en primeras 3 cards VENTAS
-for (let i = 0; i < 3 && i < propiedadesVenta.length; i++) {
-    const datosApartamento = propiedadesVenta[i]
-
-// card HTML
-    const tarjetaHTML =
-     `
+        // card HTML
+        const tarjetaHTML =
+            `
         <div class="col-md-4 mb-4">
             <div class="card">
                 <img src="${datosApartamento.imagen}" class="card-img-top" alt="Imagen del departamento">
                 <div class="card-body">
                     <h5 class="card-title">${datosApartamento.titulo}</h5>
                     <p class="card-text">${datosApartamento.descripcion}</p>
-                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.direccion}</p>
-                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.banos} Baños</p>
-                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.precio}</p>
+                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.ubicacion}</p>
+                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.baños} Baños</p>
+                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.costo}</p>
                     ${generarMensajeYClases(datosApartamento.smoke, datosApartamento.smoke ? 'fa-smoking' : 'fa-smoking-ban', 'Permitido fumar', 'No se permite fumar', 'text-success', 'text-danger')}
                     ${generarMensajeYClases(datosApartamento.pets, datosApartamento.pets ? 'fa-paw' : 'fa-ban', 'Mascotas permitidas', 'No se permiten mascotas', 'text-success', 'text-danger')}
                 </div>
             </div>
         </div>
-    `
-// Añadir HTML al contenedor
-    ventaMuestra.innerHTML += tarjetaHTML        
+    `;
 
+        // Añadir HTML al contenedor
+        contenedor.innerHTML += tarjetaHTML;
+    }
 }
 
+document.addEventListener("DOMContentLoaded", function () {
+    agregarTarjetasVenContenedor(propiedadesVenta, ventaMuestra);
+});
 
-// iTERAR PRIMERAS 3 CARDS ALQUILER
-for (let i = 0; i < 3 && i < propiedadesAlquiler.length; i++) {
-    const datosApartamento = propiedadesAlquiler[i]
+//agregar 3 tarjetas Alquiler
+function agregarTarjetasAlContenedor(datos, contenedor) {
+    for (let i = 0; i < 3 && i < datos.length; i++) {
+        const datosApartamento = datos[i];
 
-    // card HTML
-    const tarjetaHTML =
-     `
+        // card HTML
+        const tarjetaHTML =
+            `
         <div class="col-md-4 mb-4">
             <div class="card">
                 <img src="${datosApartamento.imagen}" class="card-img-top" alt="Imagen del departamento">
                 <div class="card-body">
                     <h5 class="card-title">${datosApartamento.titulo}</h5>
                     <p class="card-text">${datosApartamento.descripcion}</p>
-                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.direccion}</p>
-                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.banos} Baños</p>
-                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.precio}</p>
+                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.ubicacion}</p>
+                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.baños} Baños</p>
+                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.costo}</p>
                     ${generarMensajeYClases(datosApartamento.smoke, datosApartamento.smoke ? 'fa-smoking' : 'fa-smoking-ban', 'Permitido fumar', 'No se permite fumar', 'text-success', 'text-danger')}
                     ${generarMensajeYClases(datosApartamento.pets, datosApartamento.pets ? 'fa-paw' : 'fa-ban', 'Mascotas permitidas', 'No se permiten mascotas', 'text-success', 'text-danger')}
                 </div>
             </div>
         </div>
-    `
-    
-    // Añadir HTML al contenedor
-    alquilerMuestra.innerHTML += tarjetaHTML
+    `;
+
+        // Añadir HTML al contenedor
+        contenedor.innerHTML += tarjetaHTML;
+    }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    agregarTarjetasAlContenedor(propiedadesAlquiler, alquilerMuestra);
+});
+
+//agregar todas las cards en pagina venta
+
+function agregarTarjetasVentaTotal(datos, contenedor) {
+    for (let i = 0; i < datos.length; i++) {
+        const datosApartamento = datos[i];
+
+        // card HTML
+        const tarjetaHTML =
+            `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${datosApartamento.imagen}" class="card-img-top" alt="Imagen del departamento">
+                <div class="card-body">
+                    <h5 class="card-title">${datosApartamento.titulo}</h5>
+                    <p class="card-text">${datosApartamento.descripcion}</p>
+                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.ubicacion}</p>
+                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.baños} Baños</p>
+                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.costo}</p>
+                    ${generarMensajeYClases(datosApartamento.smoke, datosApartamento.smoke ? 'fa-smoking' : 'fa-smoking-ban', 'Permitido fumar', 'No se permite fumar', 'text-success', 'text-danger')}
+                    ${generarMensajeYClases(datosApartamento.pets, datosApartamento.pets ? 'fa-paw' : 'fa-ban', 'Mascotas permitidas', 'No se permiten mascotas', 'text-success', 'text-danger')}
+                </div>
+            </div>
+        </div>
+    `;
+
+        // Añadir HTML al contenedor
+        contenedor.innerHTML += tarjetaHTML;
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    agregarTarjetasVentaTotal(propiedadesVenta, totalVenta);
+});
+
+//agregar todas las cards en pagina alquiler
+
+function agregarTarjetasAlquilerTotal(datos, contenedor) {
+    for (let i = 0; i < datos.length; i++) {
+        const datosApartamento = datos[i];
+
+        // card HTML
+        const tarjetaHTML =
+            `
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                <img src="${datosApartamento.imagen}" class="card-img-top" alt="Imagen del departamento">
+                <div class="card-body">
+                    <h5 class="card-title">${datosApartamento.titulo}</h5>
+                    <p class="card-text">${datosApartamento.descripcion}</p>
+                    <p><i class="fas fa-map-marker-alt"></i> ${datosApartamento.ubicacion}</p>
+                    <p><i class="fas fa-bed"></i> ${datosApartamento.habitaciones} Habitaciones | <i class="fas fa-bath"></i> ${datosApartamento.baños} Baños</p>
+                    <p><i class="fas fa-dollar-sign"></i> ${datosApartamento.costo}</p>
+                    ${generarMensajeYClases(datosApartamento.smoke, datosApartamento.smoke ? 'fa-smoking' : 'fa-smoking-ban', 'Permitido fumar', 'No se permite fumar', 'text-success', 'text-danger')}
+                    ${generarMensajeYClases(datosApartamento.pets, datosApartamento.pets ? 'fa-paw' : 'fa-ban', 'Mascotas permitidas', 'No se permiten mascotas', 'text-success', 'text-danger')}
+                </div>
+            </div>
+        </div>
+    `;
+
+        // Añadir HTML al contenedor
+        contenedor.innerHTML += tarjetaHTML;
+    }
+}
+document.addEventListener("DOMContentLoaded", function () {
+    agregarTarjetasAlquilerTotal(propiedadesAlquiler, totalAlquiler);
+});
+
